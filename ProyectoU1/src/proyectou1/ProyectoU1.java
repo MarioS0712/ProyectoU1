9690054c85c72a47c2837b152089666dafbc5b4b
MarioS0712 <s24030165@itsch.edu.mx>
2026-03-14 15:53:11 -0600

    Add files via upload

---
+++ ProyectoU1/src/proyectou1/ProyectoU1.java
@@ -0,0 +1,19 @@
+package proyectou1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mariosolis
+ */
+public class ProyectoU1 {
+    public static void main(String[] args) {
+         Ventana ventana = new Ventana();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(800,800);
+        ventana.setResizable(false);
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+    
+}
---
+++ ProyectoU1/src/proyectou1/Ventana.java
@@ -0,0 +1,350 @@
+package proyectou1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author mariosolis
+ */
+public class Ventana extends JFrame {
+
+    private JPanel panelNorte, panelOeste, panelCentro, panelEste, panelSur;
+    private JPanel panelOeste2, panelCentro2, panelEste2, panelSur2;
+    private JTextField txtNombre;
+    private JRadioButton rbPerro, rbGato, rbAve;
+    private JCheckBox cbBaño, cbCorte, cbVacunas, cbShampoo, cbUñas, cbCollar;
+    private JLabel Mascota, Tipo, Servicio, Extras, mascotaImg;
+    private String mascotasImg[] = {"care.png","dog.png","cat.png","bird.png"};
+    private Icon icons[] = {
+        new ImageIcon(getClass().getResource(mascotasImg[0])),
+        new ImageIcon(getClass().getResource(mascotasImg[1])),
+        new ImageIcon(getClass().getResource(mascotasImg[2])),
+        new ImageIcon(getClass().getResource(mascotasImg[3]))
+    };
+    private JList listaServicios;
+    String registro = "";
+    String opcionRadio = "";
+    String opcionCBox = "";
+    DefaultTableModel modelo = new DefaultTableModel();
+    private Object[] o = new Object[4];
+
+    private String datos[] = new String[100];
+    private int contador = 0;
+    private Font textosTipo = new Font("Arial", Font.BOLD, 10);
+
+    public Ventana() {
+        super("Proyecto U1");
+        setLayout(new BorderLayout(10, 10));
+
+        initNorte();
+        initOeste();
+        initCentro();
+        initEste();
+        initSur();
+    }
+
+    private void initNorte() {
+        panelNorte = new JPanel(new BorderLayout());
+        panelNorte.setBackground(Color.ORANGE);
+        panelNorte.setBorder(BorderFactory.createTitledBorder("Sistema"));
+
+        JLabel titulo = new JLabel("Servicio para Mascotas y PetShop");
+        titulo.setFont(new Font("Arial", Font.BOLD, 18));
+        JButton btnPest1 = new JButton("Servicios");
+        Dimension dimension = new Dimension(100, 40);
+        btnPest1.setPreferredSize(dimension);
+        JButton btnPest2 = new JButton("Tienda");
+        btnPest2.setPreferredSize(dimension);
+        JPanel botones = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        botones.setBackground(Color.ORANGE);
+        botones.add(btnPest1);
+        botones.add(btnPest2);
+        panelNorte.add(titulo, BorderLayout.NORTH);
+        panelNorte.add(botones, BorderLayout.SOUTH);
+        add(panelNorte, BorderLayout.NORTH);
+
+        btnPest1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (panelCentro.isVisible()) {
+                    repaint();
+                } else {
+                    panelCentro2.setVisible(false);
+                    panelEste.setVisible(true);
+                    panelOeste.setVisible(true);
+                    panelSur.setVisible(true);
+                    panelCentro.setVisible(true);
+                }
+            }
+        });
+
+        btnPest2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelEste.setVisible(false);
+                panelOeste.setVisible(false);
+                panelSur.setVisible(false);
+                panelCentro.setVisible(false);
+                initCentro2();
+            }
+        });
+
+    }
+
+    private void initOeste() {
+        panelOeste = new JPanel(new GridLayout(10, 1));
+         panelOeste.setBackground(Color.ORANGE);
+        panelOeste.setBorder(BorderFactory.createTitledBorder("Servicio para Mascota y PetShop"));
+        panelOeste.setPreferredSize(new Dimension(220, 50));
+        JPanel panelMas1 = new JPanel(new GridLayout(2, 1, 20, 20));
+        JPanel panelMas2 = new JPanel(new GridLayout(4, 1));
+
+        JPanel panelMas3 = new JPanel(new GridLayout(4, 1));
+        JPanel panelMas4 = new JPanel(new GridLayout(4, 1));
+        panelMas1.add(new JLabel("Nombre:", SwingConstants.CENTER));
+        panelMas1.setBorder(BorderFactory.createTitledBorder(""));
+        txtNombre = new JTextField(5);
+        
+        panelMas1.add(txtNombre);
+        panelOeste.add(panelMas1, BorderLayout.CENTER);
+
+        panelMas2.add(new JLabel("Tipo de Mascota:", SwingConstants.CENTER), BorderLayout.NORTH);
+        panelMas2.setBorder(BorderFactory.createTitledBorder(""));
+        String tiposMascota[] = {"Perro", "Gato", "Ave"};
+        JRadioButton rbMascotas[] = new JRadioButton[3];
+        ButtonGroup grupo = new ButtonGroup();
+
+        for (int i = 0; i < tiposMascota.length; i++) {
+            rbMascotas[i] = new JRadioButton(tiposMascota[i]);
+            rbMascotas[i].setBorder(BorderFactory.createEmptyBorder(30, 0, 30, 0));
+            grupo.add(rbMascotas[i]);
+            panelMas2.add(rbMascotas[i]);
+        }
+        rbPerro = rbMascotas[0];
+        rbGato = rbMascotas[1];
+        rbAve = rbMascotas[2];
+        rbPerro.setSelected(true);
+
+        panelMas3.add(new JLabel("Servicios", SwingConstants.CENTER));
+        String servicios[] = {"Baño", "Corte", "Vacunas"};
+        JCheckBox cbServicios[] = new JCheckBox[3];
+
+        for (int i = 0; i < servicios.length; i++) {
+            cbServicios[i] = new JCheckBox(servicios[i]);
+            panelMas3.add(cbServicios[i]);
+        }
+        cbBaño = cbServicios[0];
+        cbCorte = cbServicios[1];
+        cbVacunas = cbServicios[2];
+        panelMas4.add(new JLabel("Extras", SwingConstants.CENTER), BorderLayout.NORTH);
+
+        String extras[] = {"Shampoo Especial", "Corte de Uñas", "Collar Antipulgas"};
+        JCheckBox cbExtras[] = new JCheckBox[3];
+
+        for (int i = 0; i < extras.length; i++) {
+            cbExtras[i] = new JCheckBox(extras[i]);
+            panelMas4.add(cbExtras[i], BorderLayout.SOUTH);
+        }
+        cbShampoo = cbExtras[0];
+        cbUñas = cbExtras[1];
+        cbCollar = cbExtras[2];
+        panelOeste.add(panelMas1);
+        panelOeste.add(panelMas2);
+        panelOeste.add(panelMas3);
+        panelOeste.add(panelMas4);
+        add(panelOeste, BorderLayout.WEST);
+    }
+
+    private void initCentro() {
+        panelCentro = new JPanel(new GridLayout(5, 1));
+        panelCentro.setBackground(Color.ORANGE);
+        panelCentro.setBorder(BorderFactory.createTitledBorder("Resumen del Servicio"));
+
+        Mascota = new JLabel("Mascota: ");
+        Tipo = new JLabel("Tipo: ");
+        Servicio = new JLabel("Servicios: ");
+        Extras = new JLabel("Extras: ");
+        mascotaImg = new JLabel(icons[0]);
+        panelCentro.add(Mascota);
+        panelCentro.add(Tipo);
+        panelCentro.add(Servicio);
+        panelCentro.add(Extras);
+        panelCentro.add(mascotaImg);
+        add(panelCentro, BorderLayout.CENTER);
+    }
+
+    private void initCentro2() {
+        panelCentro2 = new JPanel(new GridLayout(5, 1));
+        panelCentro2.setBackground(Color.ORANGE);
+        panelCentro2.setBorder(BorderFactory.createTitledBorder("Funciones Proximas"));
+
+        JLabel Mensaje = new JLabel("Pestaña 2 ", SwingConstants.CENTER);
+        panelCentro2.add(Mensaje, BorderLayout.CENTER);
+        add(panelCentro2, BorderLayout.CENTER);
+    }
+
+    private void initEste() {
+        panelEste = new JPanel(new BorderLayout());
+        panelEste.setBackground(Color.ORANGE);
+        panelEste.setBorder(BorderFactory.createTitledBorder("Lista de Servicios"));
+        panelEste.setPreferredSize(new Dimension(300, 0));
+        JTable servicios = new JTable();
+
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Animal");
+        modelo.addColumn("Servicio");
+        modelo.addColumn("Extras");
+
+        servicios.setModel(modelo);
+
+        JScrollPane scrollPane = new JScrollPane(servicios);
+        panelEste.add(scrollPane, BorderLayout.CENTER);
+
+        add(panelEste, BorderLayout.EAST);
+    }
+
+    private void initSur() {
+        panelSur = new JPanel(new FlowLayout());
+        panelSur.setBackground(Color.ORANGE);
+        panelSur.setBorder(BorderFactory.createTitledBorder("Opciones"));
+
+        JButton btnGuardar = new JButton("Guardar");
+        JButton btnAñadir = new JButton("Añadir");
+        JButton btnSalir = new JButton("Salir");
+
+        panelSur.add(btnGuardar);
+        panelSur.add(btnAñadir);
+        panelSur.add(btnSalir);
+
+        btnGuardar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String nombre = txtNombre.getText();
+                String tipo = "";
+                if (rbPerro.isSelected()) {
+                    tipo = "Perro";
+                    mascotaImg.setIcon(icons[1]);
+                }
+                if (rbGato.isSelected()) {
+                    tipo = "Gato";
+                    mascotaImg.setIcon(icons[2]);
+                }
+                if (rbAve.isSelected()) {
+                    tipo = "Ave";
+                    mascotaImg.setIcon(icons[3]);
+                }
+
+                String servicios = "";
+                if (cbBaño.isSelected()) {
+                    servicios += "Baño ";
+                }
+                if (cbCorte.isSelected()) {
+                    servicios += "Corte ";
+                }
+                if (cbVacunas.isSelected()) {
+                    servicios += "Vacunas ";
+                }
+
+                String extras = "";
+                if (cbShampoo.isSelected()) {
+                    extras += "Shampoo ";
+                }
+                if (cbUñas.isSelected()) {
+                    extras += "Corte de Uñas ";
+                }
+                if (cbCollar.isSelected()) {
+                    extras += "Collar Antipulgas ";
+                }
+
+                Mascota.setText("Mascota: " + nombre);
+                Tipo.setText("Tipo: " + tipo);
+                Servicio.setText("Servicios: " + servicios);
+                Extras.setText("Extras: " + extras);
+            }
+        });
+
+        btnAñadir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                o[0] = txtNombre.getText();
+
+                if (rbPerro.isSelected()) {
+                    o[1] = "Perro ";
+                }
+
+                if (rbGato.isSelected()) {
+                    o[1] = "Gato ";
+                }
+
+                if (rbAve.isSelected()) {
+                    o[1] = registro + "Ave ";
+                }
+
+                if (cbBaño.isSelected()) {
+                    o[2] = "Baño ";
+                }
+
+                if (cbCorte.isSelected()) {
+                    o[2] = registro + "Corte ";
+                }
+
+                if (cbVacunas.isSelected()) {
+                    o[2] = "Vacunas ";
+                }
+                if(cbBaño.isSelected()& cbCorte.isSelected()){
+                    o[2] = "Baño, Corte";
+                }
+                
+                if(cbBaño.isSelected()& cbVacunas.isSelected()){
+                    o[2] = "Baño, Vacunas";
+                }
+                
+                if(cbCorte.isSelected()& cbVacunas.isSelected()){
+                    o[2] = "Corte, Vacunas";
+                }
+                if(cbBaño.isSelected()& cbCorte.isSelected()&cbVacunas.isSelected()){
+                    o[2] = "Baño, Corte, Vacunas";
+                }
+               
+
+                if (cbShampoo.isSelected()) {
+                    o[3] = "Shampoo ";
+                }
+
+                if (cbUñas.isSelected()) {
+                    o[3] = "Corte de Uñas ";
+                }
+
+                if (cbCollar.isSelected()) {
+                    o[3] ="Collar Antipulgas ";
+                }
+                if(cbShampoo.isSelected()& cbUñas.isSelected()){
+                    o[3] = "Shampoo, Uñas";
+                }
+                
+                if(cbShampoo.isSelected()& cbCollar.isSelected()){
+                    o[3] = "Shampoo, Collar";
+                }
+                
+                if(cbUñas.isSelected()& cbCollar.isSelected()){
+                    o[3] = "Uñas, Collar";
+                }
+                if(cbShampoo.isSelected()& cbUñas.isSelected()&cbCollar.isSelected()){
+                    o[3] = "Shampoo, Uñas, Collar";
+                }
+
+                modelo.addRow(o);
+                txtNombre.setText("");
+            }
+        });
+        btnSalir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        add(panelSur, BorderLayout.SOUTH);
+    }
+}
